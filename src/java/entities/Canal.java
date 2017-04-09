346f19088d16e768a3a14c912304f79e85e513f9
eduardo <samue@ultrasupremo-pc>
2017-04-09 13:10:30 -0300

    correção da classe canal

--- src/java/entities/Canal.java
+++ src/java/entities/Canal.java
@@ -7,6 +7,34 @@ package entities;
  */
 public class Canal 
 {
-    int numeroCanal;
-    String descriçãoCanal;
+    private int numeroCanal;
+    private String descriçãoCanal;
+
+    /**
+     * @return the numeroCanal
+     */
+    public int getNumeroCanal() {
+        return numeroCanal;
+    }
+
+    /**
+     * @param numeroCanal the numeroCanal to set
+     */
+    public void setNumeroCanal(int numeroCanal) {
+        this.numeroCanal = numeroCanal;
+    }
+
+    /**
+     * @return the descriçãoCanal
+     */
+    public String getDescriçãoCanal() {
+        return descriçãoCanal;
+    }
+
+    /**
+     * @param descriçãoCanal the descriçãoCanal to set
+     */
+    public void setDescriçãoCanal(String descriçãoCanal) {
+        this.descriçãoCanal = descriçãoCanal;
+    }
 }
