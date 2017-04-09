6a2f6f9ad8c49fcddaa3946ee9155c9b1d729103
eduardo <samue@ultrasupremo-pc>
2017-04-09 12:12:49 -0300

    correção da implementação da classe cliente

--- src/java/entities/Cliente.java
+++ src/java/entities/Cliente.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,19 +9,43 @@ import java.util.List;
  * @author Eduardo
  */
 public class Cliente {
-    
+
     private String nome;
-    
-    public List<String> getNomeSeparado(){
+    private String nomeDoMeio;
+    private String sobreNome;
+
+    public Cliente(String nomeCompleto) {
+        setNomeCompleto(nomeCompleto);
+    }
+
+    private void setNomeCompleto(String nomeCompleto) {
+        List<String> nomeLista = getNomeSeparado(nomeCompleto);
+
+        int tnome = nomeLista.size();
+        nome = nomeLista.get(0);
+        sobreNome = nomeLista.get(tnome - 1);
+
+        nomeDoMeio = "";
+        for (int i = 1; i < tnome - 1; ++i) {
+            nomeDoMeio += String.format("%s ", nomeLista.get(i));
+        }
+
+        nomeDoMeio = nomeDoMeio.trim();
+
+    }
+
+    private List<String> getNomeSeparado(String nomeCompleto) {
         return Arrays.asList(nome.split("\\s"));
     }
-    
-    public String getNomeConcatenado(List<String> nomeSeparado){
+
+    public String getNomeCompleto() {
         StringBuilder sb = new StringBuilder();
-        for(String n: nomeSeparado)
-            sb.append(n).append(" ");
+        sb.append(nome)
+                .append(" ")
+                .append(nomeDoMeio)
+                .append(" ")
+                .append(sobreNome);
         return sb.toString().trim();
     }
-            
-    
+
 }
