91a9c1c64348e762b917bff6272a5c66aa5b4d72
eduardo <samue@ultrasupremo-pc>
2017-04-09 12:59:55 -0300

    criação da classe de utilitário

---
+++ src/java/program/Util.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program;
+
+/**
+ *
+ * @author samue
+ */
+public class Util {
+
+    public static void listaMenu() {
+        System.out.println("[1] Consultar dados de cliente");
+        System.out.println("[2] Consultar canais no plano");
+        System.out.println("[3] Consultar programas de um canal");
+        System.out.println("[4] Adquirir canal");
+        System.out.println("[5] Sair.");
+    }
+}
